b50b56b7613080d43953961d0bb1f3dbbdf0a9a6
Jakub Grzywaczewski <grzywaczewski.jakub@gmail.com>
2018-04-22 14:15:35 +0200

    About and Score components added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import 'bulma/css/bulma.css'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Quiz from './components/Quiz';
+import "bulma/css/bulma.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Score from "./components/Score";
+import Quiz from "./components/Quiz";
 
-ReactDOM.render(<Quiz />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/" component={Quiz} />
+            <Route path="/score" component={Score} />
+            <Route path="/about" component={About} />
+        </div>
+    </Router>,
+    document.getElementById("root")
+);
